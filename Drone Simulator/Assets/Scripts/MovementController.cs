b9e07f62e009d2afa1b138f2fe4da73bfa6a5662
zjjc123 <zjjc123@hotmail.com>
2019-02-05 13:46:56 -0800

    Added basic project with controller input
    The movement script does not work

---
+++ Drone Simulator/Assets/Scripts/MovementController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float movementSpeed = 1.0f;
+
+    [SerializeField]
+    private float verticalSpeed = 1.0f;
+
+    [SerializeField]
+    private float rotationSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("VerticalLeft") * movementSpeed * Time.deltaTime;
+        float depth = Input.GetAxis("VerticalRight") * verticalSpeed * Time.deltaTime;
+        float horizontal = Input.GetAxis("HorizontalRight") * movementSpeed * Time.deltaTime;
+        float yaw = Input.GetAxis("HorizontalLeft") * rotationSpeed * Time.deltaTime;
+
+        transform.Translate(0, vertical, 0);
+        transform.Translate(depth, 0, 0);
+        transform.Translate(0, 0, horizontal);
+
+        transform.Rotate(0, yaw, 0);
+    }
+}
