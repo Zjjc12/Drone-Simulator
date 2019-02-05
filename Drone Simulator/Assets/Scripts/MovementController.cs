21b881be1b07acd5676531f863ce5ec04caba60c
zjjc123 <zjjc123@hotmail.com>
2019-02-05 14:17:53 -0800

    Added basic movement and camera tilt

--- Drone Simulator/Assets/Scripts/MovementController.cs
+++ Drone Simulator/Assets/Scripts/MovementController.cs
@@ -14,11 +14,10 @@ public class MovementController : MonoBehaviour
     [SerializeField]
     private float rotationSpeed = 1.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject gimbalCamera;
+    [SerializeField]
+    private float cameraSpeed = 10.0f;
 
     // Update is called once per frame
     void Update()
@@ -27,11 +26,15 @@ public class MovementController : MonoBehaviour
         float depth = Input.GetAxis("VerticalRight") * verticalSpeed * Time.deltaTime;
         float horizontal = Input.GetAxis("HorizontalRight") * movementSpeed * Time.deltaTime;
         float yaw = Input.GetAxis("HorizontalLeft") * rotationSpeed * Time.deltaTime;
+        float gimbal = Input.GetAxis("Camera") * cameraSpeed * Time.deltaTime;
+
 
         transform.Translate(0, vertical, 0);
         transform.Translate(depth, 0, 0);
         transform.Translate(0, 0, horizontal);
 
         transform.Rotate(0, yaw, 0);
+        gimbalCamera.transform.Rotate(gimbal, 0, 0);
+
     }
 }
